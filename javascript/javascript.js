112f3bc5e6104483c4f3e589303b7d5d29c72627
Phung <thezootree@gmail.com>
2025-07-31 16:11:51 +0700

    update responsive Mobile and Tablet version_1

---
+++ javascript/javascript.js
@@ -0,0 +1,8 @@
+const getMenuSmall = document.querySelector('.icon-menu-small');
+const getNavSmall = document.querySelector('.nav__item-small');
+
+
+getMenuSmall.addEventListener('click', function() {
+    getNavSmall.classList.toggle('open');
+    getNavSmall.classList.toggle('close');
+})
